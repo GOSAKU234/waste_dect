5c8694f7bf6001b0b337d42f41ffca5a54753422
shubhan s <gokumodd05@gmail.com>
2023-12-29 19:16:41 +0530

    files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Shubhan S',
+    author_email= 'gokumodd05@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
